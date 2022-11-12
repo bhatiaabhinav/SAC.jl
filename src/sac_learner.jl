b2478a72cf9d8dd65c20ba7af945ef29886c4b2e
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2022-11-12 01:30:21 -0500

    accept gamma as argument to sac learner.

--- src/sac_learner.jl
+++ src/sac_learner.jl
@@ -5,6 +5,7 @@ export SACLearner
 mutable struct SACLearner{Tₛ<:AbstractFloat, Tₐ<:AbstractFloat} <: AbstractHook
     π::SACPolicy{Tₛ, Tₐ}
     critics
+    γ::Float32
     α::Float32
     ρ::Float32
     min_explore_steps::Int
@@ -17,9 +18,11 @@ mutable struct SACLearner{Tₛ<:AbstractFloat, Tₐ<:AbstractFloat} <: AbstractH
     optim_actor::Adam
     optim_critics::Adam
 
-    function SACLearner(π::SACPolicy{Tₛ, Tₐ}, critic, α, η_actor, η_critic; polyak=0.995, min_explore_steps=10000, train_interval=1, batch_size=32, buffer_size=1000000) where {Tₛ <: AbstractFloat, Tₐ <: AbstractFloat}
+    stats::Dict{Symbol, Float32}
+
+    function SACLearner(π::SACPolicy{Tₛ, Tₐ}, critic, γ::Real, α, η_actor, η_critic; polyak=0.995, min_explore_steps=10000, train_interval=1, batch_size=32, buffer_size=1000000) where {Tₛ <: AbstractFloat, Tₐ <: AbstractFloat}
         buff = CircularBuffer{Tuple{Vector{Tₛ}, Vector{Tₐ}, Float64, Vector{Tₛ}, Bool}}(buffer_size)
-        new{Tₛ, Tₐ}(π, (critic, deepcopy(critic)), α, polyak, min_explore_steps, train_interval, batch_size, nothing, buff, (deepcopy(critic), deepcopy(critic)), Adam(η_actor), Adam(η_critic))
+        new{Tₛ, Tₐ}(π, (critic, deepcopy(critic)), γ, α, polyak, min_explore_steps, train_interval, batch_size, nothing, buff, (deepcopy(critic), deepcopy(critic)), Adam(η_actor), Adam(η_critic), Dict{Symbol, Float32}())
     end
 end
 
@@ -27,10 +30,10 @@ function prestep(sac::SACLearner; env::AbstractMDP, kwargs...)
     sac.s = copy(state(env))
 end
 
-function poststep(sac::SACLearner{Tₛ, Tₐ}; env::AbstractMDP{Vector{Tₛ}, Vector{Tₐ}}, steps::Int, rng::AbstractRNG, kwargs...) where {Tₛ, Tₐ}
-    @unpack π, critics, α, ρ, batch_size, s, critics′ = sac
+function poststep(sac::SACLearner{Tₛ, Tₐ}; env::AbstractMDP{Vector{Tₛ}, Vector{Tₐ}}, steps::Int, returns, rng::AbstractRNG, kwargs...) where {Tₛ, Tₐ}
+    @unpack π, critics, γ, α, ρ, batch_size, s, critics′ = sac
 
-    a, r, s′, d, γ = copy(action(env)), reward(env), copy(state(env)), in_absorbing_state(env), discount_factor(env)
+    a, r, s′, d = copy(action(env)), reward(env), copy(state(env)), in_absorbing_state(env)
     push!(sac.buff, (s, a, r, s′, d))
 
     if steps >= sac.min_explore_steps && steps % sac.train_interval == 0
@@ -41,7 +44,7 @@ function poststep(sac::SACLearner{Tₛ, Tₐ}; env::AbstractMDP{Vector{Tₛ}, Ve
         𝐚′, logπ𝐚′ = sample_action_logπ(π, rng, 𝐬′)
         𝐪′ = min.(map(critic -> critic(vcat(𝐬′, 𝐚′))[1, :], critics′)...)
         𝐯′ = 𝐪′ - α * logπ𝐚′
-        𝐪 = 𝐫 + (1 .- 𝐝) * Float32(γ) .* 𝐯′
+        𝐪 = 𝐫 + (1 .- 𝐝) * γ .* 𝐯′
         ϕ = Flux.params(critics...)
         ℓϕ, ∇ϕℓ = Flux.Zygote.withgradient(ϕ) do
             return sum(map(critic -> Flux.mse(critic(vcat(𝐬, 𝐚))[1, :], 𝐪), critics)) / length(critics)
@@ -66,8 +69,14 @@ function poststep(sac::SACLearner{Tₛ, Tₐ}; env::AbstractMDP{Vector{Tₛ}, Ve
         for (param, param′) in zip(ϕ, ϕ′); copy!(param′, ρ * param′ + (1 - ρ) * param); end
 
         if steps % 1000 == 0
-            v̄ = -mean(ℓθ)
-            @info "learning stats" steps ℓϕ v̄ H α
+            v̄ = -ℓθ
+            sac.stats[:ℓϕ] = ℓϕ
+            sac.stats[:ℓθ] = ℓθ
+            sac.stats[:v̄] = v̄
+            sac.stats[:H] = H
+            sac.stats[:α] = α
+            episodes = length(returns)
+            @debug "learning stats" steps episodes ℓϕ ℓθ v̄ H α
         end
     end
     nothing
